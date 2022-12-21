587db1eb75bdf79a76f8b7e3133e9e8fc7029af6
Shady GOMAA <shadyahmed9719@gmail.com>
2022-12-22 00:45:26 +0200

    Automate Nop Commerce using selenium4 and cucumber

--- src/test/java/Steps/Hooks.java
+++ src/test/java/Steps/Hooks.java
@@ -33,41 +33,43 @@ public class Hooks {
 //				System.getProperty("user.dir") + "\\src\\test\\resources\\properties\\generalProperties.properties");
 //		prop = new Properties();
 //		prop.load(readProperty);
-		options = new ChromeOptions();
-		options.addArguments("--start-maximized");
-		options.addArguments("--disable-web-security");
-		options.addArguments("--no-proxy-server");
-		options.addArguments("--ignore-certificate-errors");
-		// to run headless test
+		if(getOperatingSystem().contains("Windows")) {
+			options = new ChromeOptions();
+			options.addArguments("--start-maximized");
+			options.addArguments("--disable-web-security");
+			options.addArguments("--no-proxy-server");
+			options.addArguments("--ignore-certificate-errors");
+			// to run headless test
 //		options.addArguments("--headless");
 
-		Map<String, Object> prefs = new HashMap<String, Object>();
-		prefs.put("credentials_enable_service", false);
-		prefs.put("profile.password_manager_enabled", false);
+			Map<String, Object> prefs = new HashMap<String, Object>();
+			prefs.put("credentials_enable_service", false);
+			prefs.put("profile.password_manager_enabled", false);
 
-		options.setExperimentalOption("prefs", prefs);
-		options.setExperimentalOption("excludeSwitches", new String[] { "enable-automation" });
+			options.setExperimentalOption("prefs", prefs);
+			options.setExperimentalOption("excludeSwitches", new String[]{"enable-automation"});
 
-		if (browser.equalsIgnoreCase("Firefox")) {
+			if (browser.equalsIgnoreCase("Firefox")) {
 //			System.setProperty("webdriver.gecko.driver",
 //					System.getProperty("user.dir") + prop.getProperty("firefoxdriver"));
-			// use webdrivermanager
-			WebDriverManager.firefoxdriver().setup();
-			driver = new FirefoxDriver();
-		} else if (browser.equalsIgnoreCase("Chrome")) {
+				// use webdrivermanager
+				WebDriverManager.firefoxdriver().setup();
+				driver = new FirefoxDriver();
+			} else if (browser.equalsIgnoreCase("Chrome")) {
 //			System.setProperty("webdriver.chrome.driver",
 //					System.getProperty("user.dir") + prop.getProperty("chromedriver"));
-			// use web driver manager
-			WebDriverManager.chromedriver().setup();
-			driver = new ChromeDriver(options);
-		} else {
-			throw new IllegalArgumentException("Invalid browser value!!");
-			// Change thread count 1 for sequential , 2 for parallel 3 ..browser..
-		}
+				// use web driver manager
+				WebDriverManager.chromedriver().setup();
+				driver = new ChromeDriver(options);
+			} else {
+				throw new IllegalArgumentException("Invalid browser value!!");
+				// Change thread count 1 for sequential , 2 for parallel 3 ..browser..
+			}
 
-		js = (JavascriptExecutor) driver;
-		// Set Driver wait
-		driver.manage().timeouts().implicitlyWait(40, TimeUnit.SECONDS);
+			js = (JavascriptExecutor) driver;
+			// Set Driver wait
+			driver.manage().timeouts().implicitlyWait(40, TimeUnit.SECONDS);
+		}
 	}
 
 	// Start nopcommerce demo Web Application
@@ -90,5 +92,11 @@ public class Hooks {
 		report.endTest(test);
 		report.flush();
 		driver.quit();
+	}
+	// check operating system
+	public String getOperatingSystem() {
+		String os = System.getProperty("os.name");
+		 System.out.println("Using System Property: " + os);
+		return os;
 	}
 }
